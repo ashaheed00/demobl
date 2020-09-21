0d011b8b315eb1c0e4e523d7fc855b98ec7389e0
ashaheed00 <ashaheed00@gmail.com>
2020-09-21 09:23:19 +0530

    Adding all of them

---
+++ src/main/java/com/mavenfisrt/App.java
@@ -0,0 +1,31 @@
+package com.mavenfisrt;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	private static final Logger LOG = LogManager.getLogger(App.class);
+
+	public static void main(String[] args) {
+		String message = "Hello World!";
+
+		LOG.debug(message + " wil be printed on Debug");
+		LOG.info(message + " wil be printed on Info");
+		LOG.warn(message + " wil be printed on Warn");
+		LOG.error(message + " wil be printed on Error");
+		LOG.fatal(message + " wil be printed on Fatal");
+		LOG.info("Appending String: {}.", message);
+		System.out.println("Hello World!");
+	}
+}
+
+//A Logger that is directly linked to the root LoggerConfig can be obtained as follows:
+//
+//Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+//Alternatively, and more simply:
+//
+//Logger logger = LogManager.getRootLogger();
